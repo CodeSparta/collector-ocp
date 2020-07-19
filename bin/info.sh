bb43bc8cb8e2bcc41ca4a98f612ecbdbd1103e3e
usrbinkat <kat@braincraft.io>
2020-07-18 22:38:31 -0600

    fix info

--- bin/info.sh
+++ bin/info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 info () {
-bundleDu="$(du -h /root/deploy/bundle/koffer-bundle.collector-operators.tar | awk '{print $1}' | head -n 1)"
-bundlePath="$(ls /root/deploy/bundle/koffer-bundle.collector-operators.tar | head -n 1 | sed 's/root\/deploy/tmp\/platform/')"
+bundleDu="$(du -h /root/deploy/bundle/koffer-bundle.openshift-*.tar | awk '{print $1}' | head -n 1)"
+bundlePath="$(ls /root/deploy/bundle/koffer-bundle.openshift-*.tar | head -n 1 | sed 's/root\/deploy/tmp\/platform/')"
 cat <<EOF
 
   Koffer Bundle Complete: ${bundleDu} ${bundlePath}
@@ -10,9 +10,7 @@ cat <<EOF
     - Move to cloudctl deployment services bastion \`/tmp\`
     - Unpack bundle into image registry path via cmd:
 
-        \`tar xv -f /tmp/koffer-bundle.collector-operators.tar -C /root/deploy\`
-
-    - Apply operators to cluster with configs in \`mirror/config/operators/\`
+        \`tar xv -f /tmp/koffer-bundle.openshift-*.tar -C /root\`
 
 EOF
 }
