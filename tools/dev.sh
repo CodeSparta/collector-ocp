599a300d4f768c4654e26da119991e48197601c6
usrbinkat <usrbinkat@braincraft.io>
2020-09-23 21:58:33 +0000

    enable tag enforcement

--- tools/dev.sh
+++ tools/dev.sh
@@ -5,6 +5,6 @@ sudo podman run -it --rm --pull always \
     --workdir /root/platform/iac/${project} \
     --volume $(pwd):/root/platform/iac/${project}:z \
     --name ${project} -h ${project} --entrypoint bash \
-  docker.io/codesparta/konductor
+  docker.io/codesparta/koffer:latest
 
 #   --volume $(pwd)/aws/:/root/.aws/:z \
